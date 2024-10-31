6cdb6635147c2726e17c083e84c80ed17da2cfac
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-10-31 21:15:16 +0530

    select all element

---
+++ script.js
@@ -0,0 +1,6 @@
+let hight = document.getElementById('hight');
+let wight = document.getElementById('weight');
+let button = document.getElementById('button');
+
+let scor = document.getElementById('scor');
+let result = document.getElementById('result');
