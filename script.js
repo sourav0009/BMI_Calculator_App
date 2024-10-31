a6894c454dc98f47cd532cc30c3ba61fc844d774
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-10-31 21:15:54 +0530

    creat function

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ let button = document.getElementById('button');
 
 let scor = document.getElementById('scor');
 let result = document.getElementById('result');
+
+button.addEventListener('click', function () {
+    let newHight = parseFloat(hight.value);
+    let newWight = parseFloat(wight.value);
+    newHight = newHight / 100;
+    let squHight = newHight * newHight;
+    let bmi = newWight / squHight;
+    scor.textContent = bmi.toFixed(2);
+    result.style.display = 'block';
+
+    if( scor.textContent < 18.6){
+        scor.style.background = 'yellow'
+    } else if (scor.textContent < 24.8){
+        scor.style.background = 'green'
+    }else{
+        scor.style.background = 'red'
+    }
+
+    
+});
